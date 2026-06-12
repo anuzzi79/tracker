da56c8e3d5999cee1b761137c439815cfed35a99
Antonio Nuzzi <antonionuzzi@gmail.com>
2026-06-11 23:16:26 -0300

    Persist transcription studies in Firebase

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,5 +1,7 @@
 const ALLOWED_ORIGINS = new Set([
   "https://anuzzi79.github.io",
+  "http://localhost:8000",
+  "http://127.0.0.1:8000",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
 ]);
